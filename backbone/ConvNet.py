65c1e78aa592092991da668c25abf5fc7f633b69
Yi Ni <2859477493@qq.com>
2025-12-26 16:03:03 -0500

    test

--- backbone/ConvNet.py
+++ backbone/ConvNet.py
@@ -11,12 +11,14 @@ class ConvNet(nn.Module):
             nn.Conv2d(in_dim, 32, kernel_size=3, padding=1),
             nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
+            # nn.MaxPool2d(kernel_size=2, stride=2)
+        )
         self.layer2 = nn.Sequential(
             nn.Conv2d(32, 64, kernel_size=3),
             nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.MaxPool2d(2))
+            # nn.MaxPool2d(2)
+        )
         self.fc1 = nn.LazyLinear(600)
         self.fc2 = nn.Linear(600, 120)
         self.fc3 = nn.Linear(120, out_dim)
